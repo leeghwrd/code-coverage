45692a464b3208bc576b9c9ffc3f1060fb01c929
leeghwrd <leeghwrd@gmail.com>
2020-04-30 11:16:20 -0700

    Add usage description to filterSearch function

--- src/filterSearch.js
+++ src/filterSearch.js
@@ -1,3 +1,9 @@
+/**
+  * @desc search url for matching string
+  * @param array inputArr - the array of urls
+  * @param string inputArr - the array of urls
+  * @return [object Array] - return object(s) matching regular expression
+*/
 const filterSearch = (inputArr, searchTerm) => {
   if (!searchTerm) throw Error("searchTerm cannot be empty");
   if (!inputArr.length) throw Error("inputArr cannot be empty");
