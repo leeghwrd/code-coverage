366cb61b1d5035fbd1ca648e874872ad79994c54
leeghwrd <leeghwrd@gmail.com>
2020-04-29 23:59:22 -0700

    Add project to git

---
+++ __tests__/filterSearch.spec.js
@@ -0,0 +1,53 @@
+const filterSearch = require("../src/filterSearch");
+
+describe("Filter function", () => {
+
+  // TEST 1
+  test("it should filter by a search term (link)", () => {
+    const input = [
+      { id: 1, url: "https://www.url1.dev" },
+      { id: 2, url: "https://www.url2.dev" },
+      { id: 3, url: "https://www.link3.dev" }
+    ];
+    const output = [{ id: 3, url: "https://www.link3.dev" }];
+    expect(filterSearch(input, "link")).toEqual(output);
+    expect(filterSearch(input, "LINK")).toEqual(output);
+  });
+  
+  // TEST 2
+  test("it should filter by a search term (uRl)", () => {
+    // actual test
+    const input = [
+      { id: 1, url: "https://www.url1.dev" },
+      { id: 2, url: "https://www.url2.dev" },
+      { id: 3, url: "https://www.link3.dev" }
+    ];
+    const output = [
+      { id: 1, url: "https://www.url1.dev" },
+      { id: 2, url: "https://www.url2.dev" }
+    ];
+    expect(filterSearch(input, "uRl")).toEqual(output);
+  
+  });
+
+  //TEST 3
+  test("it should throw when searchTerm is empty string", () => {
+    const input = [
+      { id: 1, url: "https://www.url1.dev" },
+      { id: 2, url: "https://www.url2.dev" },
+      { id: 3, url: "https://www.link3.dev" }
+    ];
+    expect(() => {
+      filterSearch(input, "");
+    }).toThrowError(Error('searchTerm cannot be empty'));
+  });
+  
+  // TEST 4
+  test("it should throw when inputArr is empty", () => {
+    const input = [];
+    expect(() => {
+      filterSearch(input, "searchTerm");
+    }).toThrowError(Error('inputArr cannot be empty'));
+  });
+
+});
